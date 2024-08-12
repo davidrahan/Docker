59ee2c2159fe50c199af6709ad3c37f541298b16
MURALIDHARAN M <86415499+davidrahan@users.noreply.github.com>
2024-08-12 16:05:26 +0530

    Update HelloDocker.java
    updated

--- new1.java
+++ new1.java
@@ -1,5 +1,5 @@
 public class HelloDocker {
     public static void main(String[] args) {
-        System.out.println("Hi guys, welcome to Docker Hub");
+        System.out.println("Hi guys, welcomie to Docker Hub");
     }
 }
